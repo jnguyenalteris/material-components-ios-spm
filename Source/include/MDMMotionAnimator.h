4e4b74eba02d3c10adb5c4874e67e84d86d8075d
Johnny Nguyen <jnguyen@alterisgroup.com>
2023-10-06 22:06:36 -0400

    more imports

--- Source/include/MDMMotionAnimator.h
+++ Source/include/MDMMotionAnimator.h
@@ -18,9 +18,9 @@
 #import <QuartzCore/QuartzCore.h>
 
 #ifdef IS_BAZEL_BUILD
-#import <MotionInterchange/MotionInterchange.h>
+#import <MotionInterchange.h>
 #else
-#import <MotionInterchange/MotionInterchange.h>
+#import <MotionInterchange.h>
 #endif
 
 #import "MDMAnimatableKeyPaths.h"
